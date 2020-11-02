14e7d3fea5e874a9c5f2e773e1fc428090f6dd26
ILister1 <ilister@qa.com>
2020-11-02 11:46:40 +0000

    squared app

---
+++ app.py
@@ -0,0 +1,8 @@
+from flask import Flask
+  
+app = Flask(__name__)
+@app.route('/squared/<int:number>')
+def squared(number):
+    return str(number ** 2)
+if __name__=='__main__':
+    app.run(host='0.0.0.0', debug=True)
